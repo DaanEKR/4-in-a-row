bf8a84e63db5bfc8a855ea7ab0fd6d0504678ddb
DaanEKR <daanvanekeren@gmail.com>
2020-09-02 08:36:19 +0200

    added winner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { collectExternalReferences } from '@angular/compiler';
+
+const PLAYER_COIN = "You";
+const COMPUTER_COIN = "CPU";
 
 @Component({
   selector: 'app-root',
@@ -12,9 +16,9 @@ export class AppComponent implements OnInit {
   COLUMN_HEIGHT = 6;
 
   title = 'game';
-  isplayerWin : boolean;
-  isOpponentWin : boolean;
-  winner : boolean = false;
+  isplayerWin: boolean;
+  isOpponentWin: boolean;
+  winner: string;
   board: string[][] = [];
 
 
@@ -23,37 +27,49 @@ export class AppComponent implements OnInit {
       this.board.push([]); // add empty column
     }
   }
+  // extract last 3 items
+  // check resultive array
+  check3(column: string[], coinType: string) {
+    return column.length > 2 && column.slice(-3).every(coin => coin === coinType);
+  }
 
-  // PLAYER MOVE +
+  check4(column: string[], coinType: string) {
+    return column.length > 3 && column.slice(-4).every(coin => coin === coinType);
+  }
 
-  // PLAYER WINS?
+  playerWins() {
+    return this.board.some(column => this.check4(column, PLAYER_COIN));
+  }
 
-  // COMPUER MOVE +
+  computerWins() {
+    return this.board.some(column => this.check4(column, COMPUTER_COIN));
+  }
 
-  /// COMPUTER WINS?
-
-  check4(array) {
-    return array.some(function (a, i, aa) {
-      return i > 1 && a === aa[i - 2] && a === aa[i - 3];
-    });
+  gameOver() {
+    return this.playerWins() || this.computerWins();
   }
 
   turn({ column }) {
-    if(this.board[column].length < 6){
-      this.board[column].push('you');
-      if(this.check4(this.board[column])=== true){
-        console.log(this.board);
-        console.log("Won")
-        this.winner = !this.winner;
+    if(this.board[column].length < this.COLUMN_HEIGHT) {
+      this.board[column].push(PLAYER_COIN);
+      if (this.playerWins()){
+        this.gameOver();
+        this.winner = 'YOU WIN';
         return;
-      } else {
-        const CPUChoice = Math.floor(Math.random() * 7);
-        this.board[CPUChoice].push('CPU');
       }
+
+      const CPUChoice = Math.floor(Math.random() * 7);
+      this.board[CPUChoice].push(COMPUTER_COIN);
+      console.log(this.board);
+      if (this.computerWins()){
+        this.gameOver();
+        this.winner = 'CPU WIN';
+        return;
+      }
+
     } else {
       console.log('invalid')
     }
-    console.log(this.board);
     // COMPUTER WINS?
     // -- YES
     // --- GAME OVER
